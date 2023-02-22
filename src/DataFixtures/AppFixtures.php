080a8df06f97f9e243221da1b04a181272448c2c
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-02-22 19:38:45 +0100

    Complete first version of fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use Faker\Factory;
 use App\Entity\Run;
 use App\Entity\Admin;
 use App\Entity\Runner;
+use App\Entity\Coordinates;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -15,10 +16,12 @@ class AppFixtures extends Fixture
     private $passwordHasher;
     private $faker;
 
+    const N_RUNNERS = 10;
+
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
-        $this->faker = Factory::create();
+        $this->faker = Factory::create("fr_FR");
     }
 
     public function load(ObjectManager $manager): void
@@ -30,14 +33,17 @@ class AppFixtures extends Fixture
     public function loadUsers(ObjectManager $manager)
     {
         $admin = new Admin();
-        $runner = new Runner();
 
-        $runner
-            ->setLogin($this->faker->userName())
-            ->setPassword($this->passwordHasher->hashPassword($runner, '1234'))
-            ->setRoles(['ROLE_RUNNER'])
-            ->setPicture("image.png");
-        $manager->persist($runner);
+        for ($i = 1; $i < self::N_RUNNERS; $i++) {
+            $runner = new Runner();
+            $runner
+                ->setLogin($this->faker->userName())
+                ->setPassword($this->passwordHasher->hashPassword($runner, '1234'))
+                ->setRoles(['ROLE_RUNNER'])
+                ->setPicture("image.png");
+            $this->addReference("runner-" . $i, $runner);
+            $manager->persist($runner);
+        }
 
         $admin
             ->setLogin("superadmin")
@@ -58,6 +64,18 @@ class AppFixtures extends Fixture
             ->setRunDate(new \DateTimeImmutable());
 
         $manager->persist($run);
+
+        for ($i = 1; $i < self::N_RUNNERS; $i++) {
+            $coords = new Coordinates();
+            $coords
+                ->setRun($run)
+                ->setCoordsDate(new \DateTimeImmutable())
+                ->setLatitude($this->faker->latitude())
+                ->setLongitude($this->faker->longitude())
+                ->setRunner($this->getReference("runner-1"));
+
+            $manager->persist($coords);
+        }
         $manager->flush();
     }
 }
