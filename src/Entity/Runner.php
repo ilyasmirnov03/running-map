668837c8491e909bc569c162053f12a66d41606b
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-02-27 10:09:37 +0100

    add: auto assigning role to admin and runner in entities

--- src/Entity/Runner.php
+++ src/Entity/Runner.php
@@ -78,7 +78,7 @@ class Runner implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_RUNNER';
 
         return array_unique($roles);
     }
