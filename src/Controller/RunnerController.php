703c39698ca729d157852f8b50ee78b6741d9698
l33ass <ilua-smirnof@yandex.ru>
2023-03-02 12:27:16 +0100

    wip: adding runner feature to request participating in a race

--- src/Controller/RunnerController.php
+++ src/Controller/RunnerController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\RunRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/runner')]
 class RunnerController extends AbstractController
 {
-    #[Route('/runner', name: 'app_runner')]
-    public function index(): Response
+    #[Route('/', name: 'app_runner')]
+    public function index(RunRepository $runRepository): Response
     {
         return $this->render('runner/index.html.twig', [
-            'controller_name' => 'RunnerController',
+            'runs' => $runRepository->findAll(),
+            'user' => $this->getUser()
         ]);
     }
 }
