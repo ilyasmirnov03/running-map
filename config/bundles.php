b5cfcfeca32c73ef5f4ed60f47958ea12027c8d9
l33ass <ilua-smirnof@yandex.ru>
2023-03-07 10:58:31 +0100

    remove: reset password package

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
