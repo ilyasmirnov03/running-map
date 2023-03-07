ac42783deeeff7099328c4cb179a1843ab0ab1d7
l33ass <ilua-smirnof@yandex.ru>
2023-03-07 16:37:55 +0100

    add: getting coords from kml file

--- src/Controller/admin/AdminController.php
+++ src/Controller/admin/AdminController.php
@@ -22,18 +22,31 @@ class AdminController extends AbstractController
             'requests' => $runJoinRequestRepository->findAll()
         ]);
     }
+    
     #[Route('/accept/{id}', name: 'app_admin_accept', methods: ['POST'])]
     public function acceptRequest(EntityManagerInterface $manager, Run $run, RunnerRepository $runnerRepository, RunJoinRequestRepository $runJoinRequestRepository): RedirectResponse
     {
         $req = Request::createFromGlobals();
-
+        
         if ($req->request->get('accept')) {
             $run->addRunner($runnerRepository->find($req->request->get('runnerId')));
             $manager->persist($run);
             $manager->flush();
         }
-
+        
         $runJoinRequestRepository->remove($runJoinRequestRepository->find($req->request->get('request')), true);
         return $this->redirectToRoute('app_admin_index');
+    }
+    
+    #[Route('/map', name: 'app_admin_map', methods: ['GET'])]
+    public function map(): Response
+    {
+        $map = \simplexml_load_file($this->getParameter('map_directory') . "/default.kml");
+        $lines = ((array) $map->Document)["Placemark"];
+        $coords = array();
+        foreach ($lines as $coord) {
+            array_push($coords, explode("\n", trim(strval($coord->LineString->coordinates))));
+        }
+        return new Response(dump($coords));
     }
 }
