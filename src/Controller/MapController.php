d2789e87675217fe56058e8862bb6134860afce3
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-03-08 22:47:51 +0100

    add: template to see run progress with a slider

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Run;
+use App\Repository\CoordinatesRepository;
+use App\Service\ToolboxService;
+use LDAP\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,10 +24,25 @@ class MapController extends AbstractController
     #[Route('/{id}', name: 'app_map_id')]
     public function run(Run $run): Response
     {
-        return $this->render('map/index.html.twig', [
-            'controller_name' => 'MapController',
+        return $this->render('map/show.html.twig', [
             'run' => $run
         ]);
     }
+
+    #[Route('/coords/{id}/{timestamp}', name: 'app_map_coords')]
+    public function coords(Run $run, CoordinatesRepository $coordinatesRepository, ToolboxService $toolboxService, $timestamp): Response
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $runners = array();
+        $coords = array();
+        foreach ($run->getRunners() as $runner) {
+            foreach ($coordinatesRepository->findBy(["run" => $run, "runner" => $runner]) as $coord) {
+                array_push($coords, ["latitude" => $coord->getLatitude(), "longitude" => $coord->getLongitude(), "date" => $coord->getCoordsDate()->format('U')]);
+            }
+            array_push($runners, ["runner" => $runner->getId(), "coords" => $toolboxService->find_closest($coords, $timestamp)]);
+        }
+        $response->setContent(json_encode($runners));
+        return $response;
+    }
 }
-?>
