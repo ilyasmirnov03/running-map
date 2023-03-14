3a61fda10a430901d205a6ac3af924f498ce6dcc
Guillaume <guillaume.farbos33@gmail.com>
2023-03-14 11:23:03 +0100

    Update Notification close design

--- public/assets/js/close.js
+++ public/assets/js/close.js
@@ -1,16 +1,19 @@
 close = document.querySelector(".close");
 notifButton = document.querySelector(".notifications");
 notifPopup = document.querySelector(".gest-notif");
+bg = document.querySelector(".notif-bg");
 
 console.log(close);
 console.log(notifButton);
 console.log(notifPopup);
+console.log(bg);
 
 notifButton.addEventListener("click", OpenNotif);
 
 function OpenNotif() {
   notifPopup.style.display = "flex";
   notifPopup.style.opacity = "1";
+  bg.style.display = "block";
 }
 
 close.addEventListener("click", CloseNotif);
@@ -18,4 +21,7 @@ close.addEventListener("click", CloseNotif);
 function CloseNotif() {
   notifPopup.style.display = "none";
   notifPopup.style.opacity = "0";
+  bg.style.display = "none";
 }
+
+bg.addEventListener("click", CloseNotif);
